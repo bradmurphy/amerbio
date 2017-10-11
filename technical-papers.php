cc5e71a40173f128832e239d17fbf306e003de08
Herb Torres <herb.torres@byhook.com>
2017-10-11 01:48:07 -0400

    whip layout theme - technical-papers page

---
+++ technical-papers.php
@@ -0,0 +1,90 @@
+<?php 
+
+  /* Template Name: Technical Papers Page */
+
+?>
+
+<?php include 'header.php';?>
+
+
+
+
+  <section class="technical-papers">
+    
+    <div class="section-hero">
+      
+      <div class="section-hero__content">
+        <h3 class="section-hero__headline--sml">The Technology</h3>
+        <h1 class="section-hero__headline">Technical Papers</h1>
+      </div>
+    </div>
+
+    <div class="site-section">
+    	<p class="site-copy">
+    		These technical papers were authored by ABO scientists, physicians, and others and published in respected journals. They were selected from over 20 papers and speak to the science behind our work.
+    	</p>
+    	<a href="#" class="site-copy--link">READ OUR SELECTED CLINICAL PAPERS</a>
+    </div>
+
+    <div class="section-content section-content--lrg">
+    	<div class="section-content__col">
+    		<p class="site-copy site-copy--lrg">
+    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    		</p>
+    		<p class="site-copy">
+    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    		</p>
+    		<p class="site-copy">
+    			Optics Express, 20(18), 19643-19657 (2012).
+    		</p>
+    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
+    	</div>
+
+    	<div class="section-content__col">
+    		<p class="site-copy site-copy--lrg">
+    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    		</p>
+    		<p class="site-copy">
+    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    		</p>
+    		<p class="site-copy">
+    			Optics Express, 20(18), 19643-19657 (2012).
+    		</p>
+    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
+    	</div>
+
+    	<div class="section-content__col">
+    		<p class="site-copy site-copy--lrg">
+    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    		</p>
+    		<p class="site-copy">
+    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    		</p>
+    		<p class="site-copy">
+    			Optics Express, 20(18), 19643-19657 (2012).
+    		</p>
+    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
+    	</div>
+
+    	<div class="section-content__col">
+    		<p class="site-copy site-copy--lrg">
+    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    		</p>
+    		<p class="site-copy">
+    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    		</p>
+    		<p class="site-copy">
+    			Optics Express, 20(18), 19643-19657 (2012).
+    		</p>
+    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
+    	</div>
+
+
+
+    </div>
+
+
+  </section>
+
+
+<?php include 'footer.php' ?>
