7e9b52e11d9d00e7acabe077690216f5b9595ffe
Herb Torres <herb.torres@byhook.com>
2017-10-12 23:15:41 -0400

    adds some technology papers

--- technology-papers.php
+++ technology-papers.php
@@ -9,7 +9,7 @@
 
 
 
-  <section class="technical-papers">
+<section class="technology-papers">
     
     <div class="section-hero">
       
@@ -19,15 +19,15 @@
       </div>
     </div>
 
-    <div class="site-section">
+    <div class="site-section section-hero__secondary">
     	<p class="site-copy">
     		These technical papers were authored by ABO scientists, physicians, and others and published in respected journals. They were selected from over 20 papers and speak to the science behind our work.
     	</p>
-    	<a href="#" class="site-copy--link">READ OUR SELECTED CLINICAL PAPERS</a>
+    	<a href="#" class="site-copy site-copy--link">READ OUR SELECTED CLINICAL PAPERS</a>
     </div>
 
     <div class="section-content section-content--lrg">
-    	<div class="section-content__col">
+    	<article class="section-content__article">
     		<p class="site-copy site-copy--lrg">
     			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
     		</p>
@@ -37,54 +37,53 @@
     		<p class="site-copy">
     			Optics Express, 20(18), 19643-19657 (2012).
     		</p>
-    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
-    	</div>
+    		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+    	</article>
 
-    	<div class="section-content__col">
+    	<article class="section-content__article">
     		<p class="site-copy site-copy--lrg">
-    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    			“Coherent Backscattering Spectroscopy”
     		</p>
     		<p class="site-copy">
-    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    			by Y. L. Kim, Y. Liu, H.K. Roy, R.K. Wali, and V. Backman.
     		</p>
     		<p class="site-copy">
-    			Optics Express, 20(18), 19643-19657 (2012).
+    			Optics Letters, 29(16), 1906-1908 (2004).
     		</p>
-    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
-    	</div>
+    		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+    	</article>
 
-    	<div class="section-content__col">
+    	<article class="section-content__article">
+
     		<p class="site-copy site-copy--lrg">
-    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    			Assessment of Blood Supply in Superficial Tissue using Polarization Gated Elastic Light Scattering Spectroscopy
     		</p>
     		<p class="site-copy">
-    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    			by M. P. Siegel, Y. L. Kim, H. Roy, R. Wali, V. Backman.
     		</p>
     		<p class="site-copy">
-    			Optics Express, 20(18), 19643-19657 (2012).
+    			Applied Optics, 40(2), 335-342 (2006).
     		</p>
-    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
-    	</div>
+    		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+    	</article>
 
-    	<div class="section-content__col">
+    	<article class="section-content__article">
     		<p class="site-copy site-copy--lrg">
-    			A fiber optic probe design to measure depth-limited optical properties in vivo with Low-coherence Enhanced Backscattering (LEBS) Spectroscopy
+    			Low-coherence enhanced backscattering (LEBS): principles and applications for colon cancer screening
     		</p>
     		<p class="site-copy">
-    			by N. Mutyal, A. Radosevich, B. Gould, J.D. Rogers, A. Gomes, V. Turzhitsky, V. Backman.
+    			by Y. L. Kim, V. Turzhitsky, Y. Liu, H. K. Roy, R. K. Wali, V. Backman.
     		</p>
     		<p class="site-copy">
-    			Optics Express, 20(18), 19643-19657 (2012).
+    			J. Biomed. Optics, 11(4), 041125-1-10 (2006).
     		</p>
-    		<a href="#" class="site-copy--link">DOWNLOAD PDF</a>
-    	</div>
-
-
+    		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+    	</article>
 
     </div>
 
 
-  </section>
+</section>
 
 
 <?php include 'footer.php' ?>
---
+++ technology.php
@@ -0,0 +1,73 @@
+<?php 
+
+  /* Template Name: Technology Page */
+
+?>
+
+<?php include 'header.php';?>
+
+
+<section class="technology">
+    
+    <div class="section-hero">
+      
+      <div class="section-hero__content">
+        <h1 class="section-hero__headline">The Technology</h1>
+        <div class="section-hero__divider"></div>
+
+        <div class="section-hero__secondary">
+            <p class="site-copy">
+                Our cancer screening platform begins with a single focus — To create an office-based, no-prep, non-invasive screening test that can identify patients at high risk for cancer.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+        <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/icon-prism.png" alt="icon" class="section-content-icon">
+
+        <h2 class="site-section__headline">A New Method for Screening</h2>
+        
+        <div class="section-content">
+            <div class="section-content__cell">
+                <p class="site-copy">
+                    American BioOptics is pioneering a new method to screen for colon cancer through the development of a minimally intrusive optical diagnostic test. Developed with substantial research funding from the National Science Foundation and the National Institutes of Health, the company’s novel optical technology shines light inside the colon and analyzes how the reflected light interacts with the lining of the colon.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy">
+                    This interaction provides unique insight into abnormalities in healthy appearing tissue that are too small to be seen with an endoscope or microscope. Such subtle changes, including an increase in blood flow to the surface cells lining the colon and changes in the architecture of the cells themselves, can be assessed from the rectum and have been linked to an increased risk of cancer in our early human studies.
+                </p>
+            </div>  
+        </div>
+
+    </div>
+
+    <div class="site-section site-section--link">
+        <a href="#" class="site-copy site-copy--link">Read about our technology</a>
+    </div>
+
+    <div class="section-footer">
+        <div class="section-footer__content">
+            <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/icon-email.png" alt="icon" class="section-content-icon section-content-icon--email">
+            <h2 class="site-section__headline">A New Method for Screening</h2>
+
+            <p class="site-copy">
+                If you would like to learn more about our company or if you are interested in future technology trials, please sign up for our mailing list by typing your email address below.
+            </p>
+
+            <form class="section-form">
+                <input class="section-form__input" type="email" placeholder="Type your email here">
+                <button class="section-form__button">Go</button>
+            </form>
+
+            <p class="site-copy site-copy--cite">ABO will not share your email address with anyone.</p>
+        </div>
+    </div>
+
+
+</section>
+
+
+<?php include 'footer.php' ?>
