d2d188f4d68a69ab7779d0d6e8470fb9ab75971b
Herb Torres <herb.torres@byhook.com>
2017-10-13 16:34:32 -0400

    adds product data page

---
+++ product-data.php
@@ -0,0 +1,194 @@
+<?php
+
+  /* Template Name: Product Data Page */
+
+?>
+
+<?php include 'header.php';?>
+
+<section class="product-data">
+
+    <div class="section-hero">
+
+      <div class="section-hero__content">
+        <h3 class="section-hero__headline--sml">The Product</h3>
+        <h1 class="section-hero__headline">Clinical Trial Data</h1>
+        <div class="section-hero__divider"></div>
+
+        <div class="section-hero__secondary">
+            <p class="site-copy">
+                The Sensitivity and Specificity of the trials were 100% and 64%, respectively.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <p class="site-copy site-copy--lrg">Trial results</p>
+
+      <div class="section-content">
+        <div class="section-content__cell">
+            <p class="site-copy">
+                The American BioOptics team has performed both animal and human clinical trials with a primary focus on colon cancer. Published trials to date include a pilot study involving 63 average risk patients undergoing screening colonoscopy at Evanston Northwestern Healthcare, as well as animal trials on rats and mice.
+            </p>
+
+            <div class="data-sensitivity">
+              <p class="site-copy site-copy--lrg">Sensitivity <span>100%</span></p>
+            </div>
+            <div class="data-specificity">
+              <p class="site-copy site-copy--lrg">Specificity	<span>64%</span></p>
+            </div>
+            <p class="site-copy">
+              Optical detection of advanced polyp (adenoma >1 cm) vs. colonoscopy (n=63) from single rectal measurement
+            </p>
+        </div>
+
+        <div class="section-content__cell">
+            <p class="site-copy">
+              These early results are remarkable, when compared to clinically available FOBT and fecal DNA analyses, which have sensitivities of 10.8% and 18.2%, respectively.	Ongoing trials are actively improving upon these initial results.
+            </p>
+            <p class="site-copy">
+              Both the animal and human clinical trials show the ability of optical technology to predict cancer risk well before the earliest observable changes in the cells’ histology. In addition, results of the human trials showed, for the first time, the ability of ABO’s optical technology to predict risk of advanced polyps throughout the colon in both experimental colorectal cancer models and humans.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+      <div class="site-section site-section--link">
+          <a href="#" class="site-copy site-copy--link">READ ABOUT OUR TECHNOLOGY IN OUR TECHNICAL PAPERS</a>
+      </div>
+      <p class="site-copy site-copy--cite">
+        (Source: Risk-Stratification of Colon Carcinogenesis through Enhanced Backscattering (EBS) Spectroscopy Analysis of the Uninvolved Colonic Mucosa by H. K. Roy, Y. L. Kim, Y. Liu, R. K. Wali, M. J. Goldberg, V. Turhitsky, J. Horwitz, V. Backman. Clinical Cancer Research, 19(3), 961-968 (2006).)
+      </p>
+    </div>
+
+    <div class="site-section site-section--lrg">
+
+      <p class="site-copy site-copy--lrg">Selected Clinincal Papers</p>
+
+      <div class="section-content section-content--lrg">
+      	<article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Rectal Optical Markers for In-vivo Risk Stratification of Premalignant Colorectal Lesions
+      		</p>
+      		<p class="site-copy">
+      			by Radosevich AJ, Mutyal NN, Eshein A, Gould B, Nguyen TQ, Rogers JD, Goldberg MJ, Bianchi LK, Konda V, Rex DK, Yen EF, Backman V, and Roy HK.
+      		</p>
+      		<p class="site-copy">
+      			Clin Cancer Res October 1, 2015 21; 4347.
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+      	<article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			by A.J. Radosevich, N.N. Mutyal, J.D. Rogers, B. Gould, T.A. Hensing, D. Ray, V. Backman, H.K.Roy,.
+      		</p>
+      		<p class="site-copy">
+      			by Y. L. Kim, Y. Liu, H.K. Roy, R.K. Wali, and V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			PLoS One, 9(10): e110157 (2014).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+      	<article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Assessment of Blood Supply in Superficial Tissue using Polarization Gated Elastic Light Scattering Spectroscopy
+      		</p>
+      		<p class="site-copy">
+      			by Y. Liu, R. E. Brand, V. Turzhitsky, Y. L. Kim, H. K. Roy, N. Hasabou, C. Sturgis, D. Shah, C. Hall, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			National Science Foundation Press Release
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+      	<article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Spectral Markers in Preneoplastic Intestinal Mucosa: An Accurate Predictor of Tumor Risk in the MIN Mouse
+      		</p>
+      		<p class="site-copy">
+      			by H. K. Roy, Y. Kim, R. K. Wali, Y. Liu, J. Koetsier, A. Kromine, D. P. Kunte, M. J. Goldberg, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			Cancer Epidemiology, Biomarkers & Prevention, 14(7), 1639-1645 (2005).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+        <article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Four-Dimensional Elastic Light-Scattering Fingerprints as Preneoplastic Markers in the Rat Model of Colon Carcinogenesis
+      		</p>
+      		<p class="site-copy">
+      			by H. K. Roy, Y. Liu, R. Wali, Y. L. Kim, A. K. Kromine, M. J. Goldberg, and V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			Gastroenterology, 126,1071-1081 (2004).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+        <article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			In Vivo Risk Analysis of Pancreatic Cancer Through Optical Characterization of Duodenal Mucosa
+      		</p>
+      		<p class="site-copy">
+      			by N.N. Mutyal, A.J. Radosevich, S. Bajaj, V. Konda, U.D. Siddiqui, I. Waxman, M.J. Goldberg, J.D. Rogers, B. Gould, A. Eshein, S. Upadhye, A. Koons, M. Gonzalez, H.K. Roy, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			Pancreas, 44(5), 735-741 (2015). PMC4464933.
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+        <article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			“Inducible Nitric Oxide Synthase (iNOS) Mediates the Early Increase of Blood Supply (EIBS) in a Pre-dysplastic Stage of Colon Carcinogenesis”
+      		</p>
+      		<p class="site-copy">
+      			by H. K. Roy, R. K. Wali, Y. Kim, Y. Liu, J. Hart, D. P. Kunte, J. L. Koetsier, M. J. Goldberg, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			FEBS, 581 (20), 3857-3862 (2007).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+        <article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Risk-Stratification of Colon Carcinogenesis through Enhanced Backscattering (EBS) Spectroscopy Analysis of the Uninvolved Colonic Mucosa
+      		</p>
+      		<p class="site-copy">
+      			by H. K. Roy, Y. L. Kim, Y. Liu, R. K. Wali, M. J. Goldberg, V. Turhitsky, J. Horwitz, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			Clinical Cancer Research, 19(3), 961-968 (2006).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+        <article class="section-content__article">
+      		<p class="site-copy site-copy--lrg">
+      			Increased Microvascular Blood Content is an Early Event in Colon Carcinogenesis
+      		</p>
+      		<p class="site-copy">
+      			by R. K. Wali, H. K. Roy, Y. L. Kim, Y. Liu, J. L. Koetsier, D. P. Kunte, M. J. Goldberg, V. Turzhitsky, V. Backman.
+      		</p>
+      		<p class="site-copy">
+      			Gut, 54, 654-660 (2005).
+      		</p>
+      		<a href="#" class="site-copy site-copy--link">DOWNLOAD PDF</a>
+      	</article>
+
+      </div>
+    </div>
+
+</section>
+
+
+<?php include 'footer.php' ?>
