5adc684d17323378cff909174588862f69e3fad6
Herb Torres <herb.torres@byhook.com>
2017-10-14 02:59:25 -0400

    adds some company page and styles

--- company-team.php
+++ company-team.php
@@ -11,57 +11,103 @@
     <div class="section-hero">
 
       <div class="section-hero__content">
-        <h1 class="section-hero__headline">The Technology</h1>
+        <h3 class="section-hero__headline--sml">The Company</h3>
+        <h1 class="section-hero__headline">Our Team</h1>
         <div class="section-hero__divider"></div>
 
         <div class="section-hero__secondary">
             <p class="site-copy">
-                Our cancer screening platform begins with a single focus — To create an office-based, no-prep, non-invasive screening test that can identify patients at high risk for cancer.
+                ABO's well-rounded team is made up of seasoned entrepreneurs, expert physicians, and an internationally-renowned biomedical engineer.
             </p>
         </div>
       </div>
     </div>
 
     <div class="site-section">
-        <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/icon-prism.png" alt="icon" class="section-content-icon">
-
-        <h2 class="site-section__headline">A New Method for Screening</h2>
-
-        <div class="section-content">
-            <div class="section-content__cell">
-                <p class="site-copy">
-                    American BioOptics is pioneering a new method to screen for colon cancer through the development of a minimally intrusive optical diagnostic test. Developed with substantial research funding from the National Science Foundation and the National Institutes of Health, the company’s novel optical technology shines light inside the colon and analyzes how the reflected light interacts with the lining of the colon.
-                </p>
+        <div class="section-content section-content__team">
+            <div class="section-side-bar">
+                <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/team-vadim.png" alt="icon" class="section-content-icon--team">
             </div>
 
-            <div class="section-content__cell">
+            <div class="section-main">
+                <p class="site-copy site-copy--heading">
+                  Vadim Backman, Co-Founder, CTO & Chairman
+                </p>
                 <p class="site-copy">
-                    This interaction provides unique insight into abnormalities in healthy appearing tissue that are too small to be seen with an endoscope or microscope. Such subtle changes, including an increase in blood flow to the surface cells lining the colon and changes in the architecture of the cells themselves, can be assessed from the rectum and have been linked to an increased risk of cancer in our early human studies.
+                  Vadim Backman is a professor of Biomedical Engineering and the director of the Biophotonics Center at Northwestern University, as well as a Member of Professional Staff at Evanston-Northwestern. Healthcare. He is a co-founder, Chairman, and Chief Technology Officer at American BioOptics LLC.
+                </p>
+                <p class="site-copy">
+                  Professor Backman received his Ph.D. degree from the MIT and Harvard University Division of Health Sciences and Technologies. His research focuses on the development of novel optical technologies for biomedical applications.
+                <p class="site-copy">
+                  Professor Backman has received a number of prestigious awards, including the NSF CAREER award and the General Motors Cancer Research Scholar Award. He was also selected by MIT’s Technology Review magazine as one of the top 100 young innovators in the world. Professor Backman has published more than 70 peer-reviewed publications in leading scientific journals including Nature, Nature Medicine, PNAS, Physical Review, Gastroenterology, and Optics Letters.
                 </p>
             </div>
         </div>
-
     </div>
 
-    <div class="site-section site-section--link">
-        <a href="#" class="site-copy site-copy--link">Read about our technology</a>
+    <div class="site-section">
+        <div class="section-content section-content__team">
+            <div class="section-side-bar">
+                <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/team-andrew.png" alt="icon" class="section-content-icon--team">
+            </div>
+
+            <div class="section-main">
+                <p class="site-copy site-copy--heading">
+                  Andrew Cittadine, Co-Founder
+                </p>
+                <p class="site-copy">
+                  Mr. Cittadine is a successful technology entrepreneur with ten years experience in general management and senior marketing roles. He is a co-founder of American BioOptics LLC.
+                </p>
+                <p class="site-copy">
+                  Until February 2007, Mr. Cittadine served as the interim CEO for SonarMed, a development stage, venture-backed medical device company in Indianapolis. Prior to SonarMed, he was a co-founder of Sensant Corporation, a Silicon Valley medical imaging company providing diagnostic quality 3D ultrasound imaging. Mr. Cittadine helped lead the company through a successful acquisition by Siemens in June 2005. Prior to that, he held positions in semiconductor design and manufacturing at Motorola.
+                <p class="site-copy">
+                  Mr. Cittadine holds a BS and MS in Electrical Engineering from Stanford where he was elected to Tau Beta Pi. He received an MBA with distinction from the Kellogg School of Management at Northwestern University.
+                </p>
+            </div>
+        </div>
     </div>
 
-    <div class="section-footer">
-        <div class="section-footer__content">
-            <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/icon-email.png" alt="icon" class="section-content-icon section-content-icon--email">
-            <h2 class="site-section__headline">A New Method for Screening</h2>
+    <div class="site-section">
+        <div class="section-content section-content__team">
+            <div class="section-side-bar">
+                <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/team-michael.png" alt="icon" class="section-content-icon--team">
+            </div>
 
-            <p class="site-copy">
-                If you would like to learn more about our company or if you are interested in future technology trials, please sign up for our mailing list by typing your email address below.
-            </p>
 
-            <form class="section-form">
-                <input class="section-form__input" type="email" placeholder="Type your email here.">
-                <button class="section-form__button">Go</button>
-            </form>
 
-            <p class="site-copy site-copy--cite">ABO will not share your email address with anyone.</p>
+            <div class="section-main">
+                <p class="site-copy site-copy--heading">
+                  Michael Goldberg MD, Director of Clinical Relations and Co-Founder
+                </p>
+                <p class="site-copy">
+                  Dr. Goldberg is a practicing gastroenterologist, an associate professor at the Feinberg School of Medicine at Northwestern University, and the section chief of gastroenterology at Evanston Northwestern Healthcare. He is Director of Clinical Relations at American BioOptics LLC.
+                </p>
+                <p class="site-copy">
+                  Dr. Goldberg graduated with high honors from both the University of Illinois and the University of Illinois medical school. He was chief medical resident at the Westside Veteran’s Administration in Chicago and completed his GI fellowship at Tufts New England Medical Center in Boston. Dr. Goldberg has previously held academic appointments at the University of Illinois and the University of Chicago. He is a past president of the Chicago Society of Endoscopy, the Chicago GI Society, and a former Governor of Illinois for the American College of Physicians. Dr. Goldberg has been named as one of Chicago’s best doctors and has received numerous citations as one of the best doctors in the U.S. In the 90s he was part of the team that successfully lobbied Congress for the Colorectal Cancer Screening Bill.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="site-section">
+        <div class="section-content section-content__team">
+            <div class="section-side-bar">
+                <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/site-theme/assets/img/team-hemant.png" alt="icon" class="section-content-icon--team">
+            </div>
+
+            <div class="section-main">
+                <p class="site-copy site-copy--heading">
+                  Hemant K. Roy, MD, Co-Founder and Director of Clinical Research
+                </p>
+                <p class="site-copy">
+                  Dr. Roy is a clinical gastroenterologist, as well as director of gastroenterology research at Evanston-Northwestern Healthcare, co-leader of the Gastrointestinal Oncology Working Group at the Robert H. Lurie Comprehensive Cancer Center of Northwestern University, and an Associate Professor of Medicine at Northwestern’s Feinberg School of Medicine. Dr. Roy is a co-founder and Director of Clinical Research at American BioOptics LLC.
+                </p>
+                <p class="site-copy">
+                  Dr. Roy received his bachelor’s degree in Molecular Biology at Vanderbilt University and graduated with distinction from Northwestern University Medical School. He continued his internal medicine training at Harvard University and took his gastroenterology fellowship at the University of Chicago.
+                <p class="site-copy">
+                  Dr. Roy has co-authored more than 40 peer reviewed publications. His expertise in clinical research is underscored by his selection as a Deputy Editor for the Archives of Internal Medicine. Dr. Roy has received numerous awards including career development awards through the American Society of Clinical Oncology and Glaxo Institute of Digestive Health. In addition, he is a primary member and a funded principal investigator of the National Cancer Institute’s Early Detection Research Network (EDRN).
+                </p>
+            </div>
         </div>
     </div>
 
--- header-news.php
+++ header-news.php
@@ -17,34 +17,34 @@
         <span class="header__text--nav-label">The Product</span>
         <div class="header__container--nav-block--hover">
           <a href="./product.php" class="header__text--nav-header">The Product</a>
-          <a href="./clinical-trial-data.php" class="header__text--nav-link">Clinical Trial Data</a>
-          <a href="./preventing-cancer.php" class="header__text--nav-link">Preventing Cancer</a>
+          <a href="./product-data.php" class="header__text--nav-link">Clinical Trial Data</a>
+          <a href="./product-prevent.php" class="header__text--nav-link">Preventing Cancer</a>
         </div>
       </div>
       <div class="header__container--nav-block">
         <span class="header__text--nav-label">The Technology</span>
         <div class="header__container--nav-block--hover">
           <a href="./technology.php" class="header__text--nav-header">The Technology</a>
-          <a href="./detailed-explanation.php" class="header__text--nav-link">Detailed Explanation</a>
-          <a href="./tech-papers.php" class="header__text--nav-link">Tech Papers</a>
+          <a href="./technology-details.php" class="header__text--nav-link">Detailed Explanation</a>
+          <a href="./technology-papers.php" class="header__text--nav-link">Tech Papers</a>
         </div>
       </div>
       <div class="header__container--nav-block">
         <span class="header__text--nav-label">The Company</span>
         <div class="header__container--nav-block--hover">
           <a href="./company.php" class="header__text--nav-header">The Company</a>
-          <a href="./values.php" class="header__text--nav-link">Our values</a>
-          <a href="./team.php" class="header__text--nav-link">Our Team</a>
-          <a href="./jobs.php" class="header__text--nav-link">Jobs</a>
-          <a href="./contact.php" class="header__text--nav-link">Contact Us</a>
+          <a href="./company-values.php" class="header__text--nav-link">Our values</a>
+          <a href="./company-team.php" class="header__text--nav-link">Our Team</a>
+          <a href="./company-jobs.php" class="header__text--nav-link">Jobs</a>
+          <a href="./company-contact.php" class="header__text--nav-link">Contact Us</a>
         </div>
       </div>
       <div class="header__container--nav-block">
         <span class="header__text--nav-label">News &amp; Press</span>
         <div class="header__container--nav-block--hover show">
           <a href="./news" class="header__text--nav-header">News &amp; Press</a>
-          <a href="./archive" class="header__text--nav-link">News Archive</a>
-          <a href="./press" class="header__text--nav-link">Press Release</a>
+          <a href="./news-archive" class="header__text--nav-link">News Archive</a>
+          <a href="./news-press" class="header__text--nav-link">Press Release</a>
         </div>
       </div>
     </div>
