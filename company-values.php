52b5d0525d9e72190ba7664cd37a25252f1bd248
Herb Torres <herb.torres@byhook.com>
2017-10-14 02:15:08 -0400

    adds company values page and styles

---
+++ company-values.php
@@ -0,0 +1,88 @@
+<?php
+
+  /* Template Name: Company Values Page */
+
+?>
+
+<?php include 'header.php';?>
+
+<section class="company-values">
+
+    <div class="section-hero">
+
+      <div class="section-hero__content">
+        <h3 class="section-hero__headline--sml">The Company</h3>
+        <h1 class="section-hero__headline">Our Values</h1>
+        <div class="section-hero__divider"></div>
+
+        <div class="section-hero__secondary">
+            <p class="site-copy">
+                Our values reflect our goal to revolutionize screening for colorectal and other cancers. They represent enduring core principles that serve as a guide for confronting complex challenges and maintaining a resilient company culture. American BioOptics, its employees, and its contractors endeavor to make all decisions, behavior, and actions consistent with these values.
+            </p>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section">
+
+        <p class="site-copy site-copy--company">
+            Our values represent enduring core principles that serve as a guide for confronting complex challenges and maintaining a resilient company culture. American BioOptics, its employees, and its contractors endeavor to make all decisions, behavior, and actions consistent with the values enumerated below:
+        </p>
+
+        <div class="section-content">
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">People</p>
+                <p class="site-copy">
+                    American BioOptics values and appreciates people as individuals with unique desires and interests. We respect our customers, employees, colleagues, partners, and competitors. We appreciate diverse backgrounds, value modesty in our interactions, and accept honest differences of opinion. ABO values the contributions of people both inside and outside the business world.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Sincerity</p>
+                <p class="site-copy">
+                    American BioOptics encourages its employees to seek the truth and express themselves in an open and earnest manner. Establishing sincerity requires honesty, a genuine desire to be understood, the acknowledgement of shortcomings and failures, and the demonstration of loyalty to one’s peers.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Passion</p>
+                <p class="site-copy">
+                    American BioOptics encourages its employees to seek out new opportunities, to dedicate themselves to achieving success, and to be motivated to share their enthusiasm with others. The company aims to provide an environment where individuals view their role as worthwhile and noble.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Commitment</p>
+                <p class="site-copy">
+                    American BioOptics values perseverance and the determination to achieve one’s objectives. Committment is reflected in when employees have the discipline to complete tasks on time and dedicate extraordinary effort to overcome obstacles.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Knowledge</p>
+                <p class="site-copy">
+                    American BioOptics values knowledge, skill, and education. Above all, ABO appreciates the spirit to achieve these goals with a sense of curiosity, creativity and adventurousness that allow its employees to take risks and learn from their mistakes.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Strength</p>
+                <p class="site-copy">
+                    Succeeding in a fast paced environment requires energy, strength, and enough self-confidence to take risks. The company and its employees have a sense of conviction, tolerate criticism and setbacks, and have the fortitude to address difficulty.
+                </p>
+            </div>
+
+            <div class="section-content__cell">
+                <p class="site-copy site-copy--lrg site-copy--values">Merit</p>
+                <p class="site-copy">
+                    American BioOptics recognizes and rewards excellence in the performance of individual and company goals. We value individuals who have repeatedly demonstrated accomplishment in their personal and professional lives. The company strives to make decisions based on merits- after a careful, objective consideration of the issues.
+                </p>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+
+<?php include 'footer.php' ?>
